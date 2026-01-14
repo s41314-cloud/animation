b055da83a977485e1fe0c64f14ead1b66e5abbff
s41314-cloud <s41314@bj.ac.th>
2026-01-14 23:43:21 +0700

    Add files via upload

---
+++ Midterm-1-19.capx/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1768408941,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/bubble-sheet0.png",
+		"images/particles.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
