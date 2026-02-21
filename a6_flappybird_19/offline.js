e8d222c71f5d32c84aa178ef9c060893f9b14c96
s41314-cloud <s41314@bj.ac.th>
2026-02-21 23:17:45 +0700

    Add files via upload

---
+++ a6_flappybird_19/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1771690497,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/pipebottom-sheet0.png",
+		"images/pipetop-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
